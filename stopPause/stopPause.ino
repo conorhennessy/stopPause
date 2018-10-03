bcb75a5b7a8968acd33bc83c07be8ac7c63c2648
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2018-10-03 15:16:58 +0100

    Code tidy

--- stopPause/stopPause.ino
+++ stopPause/stopPause.ino
@@ -1,4 +1,4 @@
-
+// Spotify Conductor project - an Arduino project for an emergency stop button to control & pause media playback
 #include <Keyboard.h>
 
 //Set pins
@@ -11,18 +11,16 @@ int buttonState = 0;
 void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
+  // initialize the button pin as an input:
   pinMode(buttonPin, INPUT);
+  
   Keyboard.begin();
 }
 
 
-// the loop function runs over and over again forever
 void loop() {
-  // read state of push button pin
-
+  // read and check state of push button pin
   buttonState = digitalRead(buttonPin);
-
   if (buttonState == HIGH) {
     // turn LED on:
     digitalWrite(ledPin, HIGH);
